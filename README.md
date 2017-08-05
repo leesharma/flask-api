2ccc3780fbb20ff1f6c6a3706fa462d8d4980d66
Lee Sharma <lee@leesharma.com>
2017-08-05 02:17:44 -0400

    Initialize Flask repository
    Repo now contains a bare-bones API app with Heroku setup.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Funny API
+
+This app was built following the Lynda.com [Building Web APIs with Flask]
+tutorial. I built it to learn more about building APIs with Flask, testing
+Flask, design best practices, available extensions, and other important Flask
+knowledge.
+
+[Building Web APIs with Flask]: https://www.lynda.com/Flask-tutorials/Web-API-Development-Flask/521200-2.html
---
+++ quotes.py
@@ -0,0 +1,23 @@
+# Copied the first 5 by hand
+
+def funny_quotes():
+    return[{
+        "author" : "Elbert Hubbard",
+        "quote" : "Do not take life too seriously. You were never get out of it alive."
+    },
+    {
+        "author" : "Mark Twain",
+        "quote" : "Get your facts first, then you can distort them as you please."
+    },
+    {
+        "author" : "Benjamin Franklin",
+        "quote" : "Wine is constant proof that God loves us and loves to see us happy."
+    },
+    {
+        "author" : "Winston Churchill",
+        "quote" : "I may be drunk, Miss, but in the morning I will be sober and you will still be ugly."
+    },
+    {
+        "author" : "Theodore Roosevelt",
+        "quote" : "If you could kick the person in the pants responsible for most of your trouble, you wouldn't sit for a month."
+    }]
